15b92f1897d98cfde4b5438501a2564182d72308
hatvikvora <42492879+hatvikvora@users.noreply.github.com>
2020-09-15 13:22:56 +0530

    Update PRAC_1.py

--- PRAC_1.py
+++ PRAC_1.py
@@ -1,1 +1,13 @@
+import telepot
+token = '1300125276:AAH3WY-1F2vYyloTtGdXj4rbr_zMO99e8t0'
+TelegramBot = telepot.Bot(token)
+print(TelegramBot.getMe())
 
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        TelegramBot.sendMessage(chat_id.format(msg["text"]))
+
+TelegramBot.message_loop(handle)
